3f6ef2cd42950ec4c8d802524efda3dbc91ea80e
Shreyaa Kashyap <44851128+shreyaakashyap@users.noreply.github.com>
2022-06-03 20:59:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+}
+
+function draw() {
+    
+}
+
+function takeSnapshot(){
+    save("1w3image.png");
+}
